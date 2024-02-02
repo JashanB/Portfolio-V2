b263b3c935b214c51880109e5bd46cf4d54ea823
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-02 13:05:13 -0500

    adding landing page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //2 things - animated start here button that once clicked, brings up modal with about me page
+  //need modal component
+  //need content to put in modal 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top-div'>
+
+      </div>
+      <div className='middle-div'>
+        <header>Jashan Brar</header>
+      </div>
+      <div></div>
     </div>
   );
 }

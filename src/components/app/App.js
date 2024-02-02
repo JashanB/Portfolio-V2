3e043201b8c81c5086b8deaec01cf39ed5c5abb8
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-02 13:37:48 -0500

    adding landing page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import React from 'react';
+import  star from './star.jpeg';
 
 function App() {
 
   //2 things - animated start here button that once clicked, brings up modal with about me page
   //need modal component
   //need content to put in modal 
+
+  function modalClick () {
+
+  }
   return (
     <div className="App">
       <div className='top-div'>
-
+        <div className='start-div'>
+          <header>Start here</header>
+          <img src={star} className='star-tail' alt="my-star"></img>
+        </div>
       </div>
       <div className='middle-div'>
         <header>Jashan Brar</header>
       </div>
       <div></div>
+      <div className="modal-div"></div>
     </div>
   );
 }

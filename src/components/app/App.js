42e99fea57af47e9c09ab09a49b4fd8168e76e8a
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-04 11:25:10 -0500

    adding modal

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,25 +2,26 @@ import './App.css';
 import React from 'react';
 import  star from './star.jpeg';
 import start from './start.png';
+import { useState } from 'react';
 
 function App() {
-
+  const [modal, setModal] = useState("modal-closed")
   //2 things - animated start here button that once clicked, brings up modal with about me page
   //need modal component
   //need content to put in modal 
 
   function modalClick () {
-
+    setModal(state => "modal-open");
   }
   return (
     <div className="App">
       <div className='body'>
         <header>Jashan Brar</header>
         <div className='start-div'>
-          <img src={start} className='start-here' alt="my-star"></img>
+          <img src={start} className='start-here' alt="my-star" onClick={() => modalClick()}></img>
         </div>
       </div>
-      <div className="modal-div"></div>
+      <div className={modal}></div>
     </div>
   );
 }

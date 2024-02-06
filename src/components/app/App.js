eff82f06b4571ee09d7cff6ee020e3179297e186
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-06 17:44:35 -0500

    moved nav into Nav folder

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,18 +4,16 @@ import star from './star.jpeg';
 import start from './start.png';
 import { useState } from 'react';
 import nav from "./nav";
+import Nav from './nav';
 
 function App() {
   // const [modal, setModal] = useState("modal-closed")
-  const [navItemClass, setNavItemClass] = useState("nav-item-normal");
-  const [hoveredNav, setHoveredNav] = useState(null);
-  //2 things - animated start here button that once clicked, brings up modal with about me page
-  //need modal component
-  //need content to put in modal 
-  //Background - Skills - Companies/Role - Projects 
-  function modalClick() {
-    setModal(state => "modal-open");
-  }
+  // const [navItemClass, setNavItemClass] = useState("nav-item-normal");
+  // const [hoveredNav, setHoveredNav] = useState(null);
+  
+  // function modalClick() {
+  //   setModal(state => "modal-open");
+  // }
 
   function hoverNav(item) {
     setHoveredNav(state => item);
@@ -35,37 +33,7 @@ function App() {
             <p>I build efficient and scaleable digital experiences</p>
           </div>
           {/* Classname for nav to be changed depending on media dimensions - erase at below certain threshold*/}
-          <nav className=''>
-            <ul>
-              <li className={hoveredNav === 1 ? "hovered-nav-item" : "nav-item-normal"}
-                onMouseEnter={() => hoverNav(1)}
-                onMouseLeave={() => moveOffNav(1)}
-              >
-                <a href="" className='flex-nav-center'>
-                  <span></span>
-                  <span>About</span>
-                </a>
-              </li>
-              <li className={hoveredNav === 2 ? "hovered-nav-item" : "nav-item-normal"}
-                onMouseEnter={() => hoverNav(2)}
-                onMouseLeave={() => moveOffNav(2)}
-              >
-                <a href="">
-                  <span></span>
-                  <span>Experience</span>
-                </a>
-              </li>
-              <li className={hoveredNav === 3 ? "hovered-nav-item" : "nav-item-normal"}
-                onMouseEnter={() => hoverNav(3)}
-                onMouseLeave={() => moveOffNav(3)}
-              >
-                <a href="">
-                  <span></span>
-                  <span>Projects</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <Nav/>
           <ul className='external-links'>
             <li><a href='https://github.com/jashanb'><svg></svg></a></li>
             <li><a href='https://www.linkedin.com/in/jashan-brar/'><svg></svg></a></li>
--- src/components/app/nav/Nav.js
+++ src/components/app/nav/Nav.js
@@ -1,3 +1,39 @@
 import { useState } from 'react';
 import React from 'react';
 import './Nav.css';
+
+export default function Nav() {
+    return (
+        <nav className=''>
+            <ul>
+                <li className={hoveredNav === 1 ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => hoverNav(1)}
+                    onMouseLeave={() => moveOffNav(1)}
+                >
+                    <a href="" className='flex-nav-center'>
+                        <span></span>
+                        <span>About</span>
+                    </a>
+                </li>
+                <li className={hoveredNav === 2 ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => hoverNav(2)}
+                    onMouseLeave={() => moveOffNav(2)}
+                >
+                    <a href="">
+                        <span></span>
+                        <span>Experience</span>
+                    </a>
+                </li>
+                <li className={hoveredNav === 3 ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => hoverNav(3)}
+                    onMouseLeave={() => moveOffNav(3)}
+                >
+                    <a href="">
+                        <span></span>
+                        <span>Projects</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}

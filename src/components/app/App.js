5ace1190479bbf4caf87764b426db2c77a3b1680
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-06 17:28:32 -0500

    adding hover functions to nav

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,12 +7,21 @@ import { useState } from 'react';
 function App() {
   // const [modal, setModal] = useState("modal-closed")
   const [navItemClass, setNavItemClass] = useState("nav-item-normal");
+  const [hoveredNav, setHoveredNav] = useState(null);
   //2 things - animated start here button that once clicked, brings up modal with about me page
   //need modal component
   //need content to put in modal 
   //Background - Skills - Companies/Role - Projects 
   function modalClick() {
     setModal(state => "modal-open");
+  }
+
+  function hoverNav(item) {
+    setHoveredNav(state => item);
+  }
+
+  function moveOffNav(item) {
+    setHoveredNav(state => null);
   }
 
   return (
@@ -27,22 +36,28 @@ function App() {
           {/* Classname for nav to be changed depending on media dimensions - erase at below certain threshold*/}
           <nav className=''>
             <ul>
-              <li className={navItemClass} 
-              onMouseEnter={setNavItemClass(state => "nav-item-hovered")}
-              onMouseLeave={setNavItemClass(state => "nav-item-normal")}
+              <li className={hoveredNav === 1 ? "hovered-nav-item" : "nav-item-normal"}
+                onMouseEnter={() => hoverNav(1)}
+                onMouseLeave={() => moveOffNav(1)}
               >
                 <a href="" className='flex-nav-center'>
                   <span></span>
                   <span>About</span>
                 </a>
               </li>
-              <li>
+              <li className={hoveredNav === 2 ? "hovered-nav-item" : "nav-item-normal"}
+                onMouseEnter={() => hoverNav(2)}
+                onMouseLeave={() => moveOffNav(2)}
+              >
                 <a href="">
                   <span></span>
                   <span>Experience</span>
                 </a>
               </li>
-              <li>
+              <li className={hoveredNav === 3 ? "hovered-nav-item" : "nav-item-normal"}
+                onMouseEnter={() => hoverNav(3)}
+                onMouseLeave={() => moveOffNav(3)}
+              >
                 <a href="">
                   <span></span>
                   <span>Projects</span>

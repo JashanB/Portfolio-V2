a370fc4bf186c47a9311ccab5fa7db5b25328589
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-15 13:31:44 -0500

    fixed div wrapping of exp

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -69,49 +69,49 @@ function App() {
                 <h2>Experience</h2>
               </div>
               <div>
-                <div className='experience-container'>
-                  <ol className='group-list'>
-                    <li className='list-item'>
-                      <div className='list-item-grid'>
-                        {/*Around list item, greater than dimensions, show on hover*/}
-                        <div className='absolute-inset'></div>
-                        <header className='date-list-item'>2020-2021</header>
-                        <div className='list-item-main'>
-                          <h3 className='list-item-header'><span>Full Stack Developer &#183; Twenty Billion Neurons</span></h3>
-                          <p>Experience at TBN description</p>
-                          <ul className='tech-stack-flex-box'>
-                            <li className='tech-item'><div className="tech-item-div">Javascript</div></li>
-                            <li className='tech-item'><div className="tech-item-div">Ruby</div></li>
-                            <li className='tech-item'><div className="tech-item-div">HTML & CSS</div></li>
-                            <li className='tech-item'><div className="tech-item-div">Ruby on Rails</div></li>
-                            <li className='tech-item'><div className="tech-item-div">React</div></li>
-                            <li className='tech-item'><div className="tech-item-div">Google Cloud</div></li>
-                            <li className='tech-item'><div className="tech-item-div">AWS</div></li>
-                          </ul>
-                        </div>
+                {/* <div className='experience-container'> */}
+                <ol className='group-list'>
+                  <li className='list-item'>
+                    <div className='list-item-grid'>
+                      {/*Around list item, greater than dimensions, show on hover*/}
+                      <div className='absolute-inset-experience'></div>
+                      <header className='date-list-item'>2020-2021</header>
+                      <div className='list-item-main'>
+                        <h3 className='list-item-header'><span>Full Stack Developer &#183; Twenty Billion Neurons</span></h3>
+                        <p>Experience at TBN description</p>
+                        <ul className='tech-stack-flex-box'>
+                          <li className='tech-item'><div className="tech-item-div">Javascript</div></li>
+                          <li className='tech-item'><div className="tech-item-div">Ruby</div></li>
+                          <li className='tech-item'><div className="tech-item-div">HTML & CSS</div></li>
+                          <li className='tech-item'><div className="tech-item-div">Ruby on Rails</div></li>
+                          <li className='tech-item'><div className="tech-item-div">React</div></li>
+                          <li className='tech-item'><div className="tech-item-div">Google Cloud</div></li>
+                          <li className='tech-item'><div className="tech-item-div">AWS</div></li>
+                        </ul>
                       </div>
-                    </li>
-                    <li className='list-item'>
-                      <div className='list-item-grid'>
-                        {/*Around list item, greater than dimensions, show on hover*/}
-                        <div className='absolute-inset'></div>
-                        <header className='date-list-item'>2019-2020</header>
-                        <div className='list-item-main'>
-                          <h3 className='list-item-header'><span>Jr Software Engineer &#183; CarltonOne Engagement</span></h3>
-                          <p>Experience at CarltonOne description</p>
-                          <ul className='tech-stack-flex-box'>
-                            <li className='tech-item'><div className="tech-item-div">Python</div></li>
-                            <li className='tech-item'><div className="tech-item-div">SQL Management Server</div></li>
-                            <li className='tech-item'><div className="tech-item-div">Navicat GUI</div></li>
-                          </ul>
-                        </div>
+                    </div>
+                  </li>
+                  <li className='list-item'>
+                    <div className='list-item-grid'>
+                      {/*Around list item, greater than dimensions, show on hover*/}
+                      <div className='absolute-inset-experience'></div>
+                      <header className='date-list-item'>2019-2020</header>
+                      <div className='list-item-main'>
+                        <h3 className='list-item-header'><span>Jr Software Engineer &#183; CarltonOne Engagement</span></h3>
+                        <p>Experience at CarltonOne description</p>
+                        <ul className='tech-stack-flex-box'>
+                          <li className='tech-item'><div className="tech-item-div">Python</div></li>
+                          <li className='tech-item'><div className="tech-item-div">SQL Management Server</div></li>
+                          <li className='tech-item'><div className="tech-item-div">Navicat GUI</div></li>
+                        </ul>
                       </div>
-                    </li>
-                  </ol>
-                  <div className='resume'>
-                    <a href=""><span>View Full Resume</span></a>
-                  </div>
+                    </div>
+                  </li>
+                </ol>
+                <div className='resume'>
+                  <a href=""><span>View Full Resume</span></a>
                 </div>
+                {/* </div> */}
               </div>
             </section>
             <section id="projects">
@@ -123,7 +123,7 @@ function App() {
                 <ul className='group-list'>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>Social Site</span></h3>
                         <p>Instagram clone describe more</p>
@@ -138,7 +138,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>Bettr Trip</span></h3>
                         <p>A web tool to plan your trip itinerary while referencing an interactive map. Weather, travel duration, and nearby interesting points are also shown for locations. A RESTful back-end server allows users to save trips and create multiple trips at once.</p>
@@ -157,7 +157,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>Weather App</span></h3>
                         <p>Weather Forecasts for your favourite locations. Hourly forecasts for the next 48 hours and weekly and historical comparisons. WeatherApp is a mobile optimized app that uses a RESTful back-end server that saves the user's searched locations, allowing for personalized usage and reduced API calls.</p>
@@ -174,7 +174,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>Newton</span></h3>
                         <p>Pinterest-like app for learners. Express web app that allows learners to save learning resources like tutorials, blogs and videos
@@ -203,7 +203,7 @@ function App() {
                 <ul className='group-list'>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>Dalhousie Research Day (2023)</span></h3>
                         <p>Assessing Burnout in Medical Learners</p>
@@ -212,7 +212,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <h3 className='list-item-header'><span>International Journal of Stroke (2016)</span></h3>
                         <p>Technological Trends in Stroke Rehabilitation for Motor Function Recovery (Vol 11 Pg 68-68).</p>
@@ -221,7 +221,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <a href="https://www.archives-pmr.org/article/S0003-9993(16)30883-8/fulltext"></a>
                         <h3 className='list-item-header'><span>Archives of Physical Medicine and Rehabilitation (2016)</span></h3>
@@ -231,7 +231,7 @@ function App() {
                   </li>
                   <li className='list-item'>
                     <div className='list-item-grid'>
-                      <div className='absolute-inset'></div>
+                      <div className='absolute-inset-experience'></div>
                       <div className='list-item-main'>
                         <a href="https://www.archives-pmr.org/article/S0003-9993(16)30883-8/fulltext"></a>
                         <h3 className='list-item-header'><span>Evidence-Based Review of Stroke Rehabilitation (2016)</span></h3>

14515e7b5e02d237356fa3dee112844e7be3bfbb
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-17 14:37:20 -0500

    trying new svgs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -119,13 +119,14 @@ function App() {
                       Resume
                       {/* <svg xmlns="http://www.w3.org/2000/svg">
                         <foreignObject width="1rem" height="1rem">
+
                           <img src={arrow} alt="Image" width="100%" height="100%" />
                         </foreignObject>
                       </svg> */}
                       {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon">
                         <path d="M0 16 L16 0 M0 0 H16 V16" stroke="black" stroke-width="3" fill="none" />
                       </svg> */}
-                      <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                      <svg className="arrow-svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                         <g id="SVGRepo_iconCarrier">
                           <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>

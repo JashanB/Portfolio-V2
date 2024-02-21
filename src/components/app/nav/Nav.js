a0859978b88be4814cb9cb3120480ed9b95906d7
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-20 22:11:11 -0500

    2 items of nav can be active at once

--- src/components/app/nav/Nav.js
+++ src/components/app/nav/Nav.js
@@ -18,7 +18,7 @@ export default function Nav(props) {
     function hoverNav(section) {
         setHoveredNav(state => section);
     }
-//move off nav while that is active means shouldnt set state to null
+    //move off nav while that is active means shouldnt set state to null
     function moveOffNav(section) {
         // console.log('active', activeSection)
         if (activeSection !== section) {
@@ -37,7 +37,7 @@ export default function Nav(props) {
         };
     }
 
-    function selectedNavItem (section) {
+    function selectedNavItem(section) {
         //return true or false
         if (activeSection === section || hoveredNav === section) {
             return true;
@@ -59,38 +59,38 @@ export default function Nav(props) {
                 <li className={hoveredNav === 1 ? "hovered-nav-item" : "nav-item-normal"}
                     onMouseEnter={() => hoverNav('about')}
                     onMouseLeave={() => moveOffNav('about')}
-                    
+
                 >
                     <Link spy={true} smooth={true} offset={-100} duration={500} to="about" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive}>
-                        <span className={hoveredNav === 1 ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={hoveredNav === 1 ? "nav-text-elongate" : "nav-text-normal"}>About</span>
+                        <span className={selectedNavItem('about') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={selectedNavItem('about') ? "nav-text-elongate" : "nav-text-normal"}>About</span>
                     </Link>
                 </li>
                 <li className={hoveredNav === 2 ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav(2)}
-                    onMouseLeave={() => moveOffNav(2)}
+                    onMouseEnter={() => hoverNav('experience')}
+                    onMouseLeave={() => moveOffNav('experience')}
                 >
                     <Link spy={true} smooth={true} offset={-60} duration={500} to="experience" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive}>
-                        <span className={hoveredNav === 2 ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={hoveredNav === 2 ? "nav-text-elongate" : "nav-text-normal"}>Experience</span>
+                        <span className={selectedNavItem('experience') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={selectedNavItem('experience') ? "nav-text-elongate" : "nav-text-normal"}>Experience</span>
                     </Link>
                 </li>
                 <li className={hoveredNav === 3 ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav(3)}
-                    onMouseLeave={() => moveOffNav(3)}
+                    onMouseEnter={() => hoverNav('projects')}
+                    onMouseLeave={() => moveOffNav('projects')}
                 >
                     <Link spy={true} smooth={true} offset={-60} duration={500} to="projects" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive}>
-                        <span className={hoveredNav === 3 ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={hoveredNav === 3 ? "nav-text-elongate" : "nav-text-normal"}>Projects</span>
+                        <span className={selectedNavItem('projects') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={selectedNavItem('projects') ? "nav-text-elongate" : "nav-text-normal"}>Projects</span>
                     </Link>
                 </li>
                 <li className={hoveredNav === 4 ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav(4)}
-                    onMouseLeave={() => moveOffNav(4)}
+                    onMouseEnter={() => hoverNav('publications')}
+                    onMouseLeave={() => moveOffNav('publications')}
                 >
                     <Link spy={true} smooth={true} offset={0} duration={500} to="publications" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive}>
-                        <span className={hoveredNav === 4 ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={hoveredNav === 4 ? "nav-text-elongate" : "nav-text-normal"}>Publications</span>
+                        <span className={selectedNavItem('publications') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={selectedNavItem('publications') ? "nav-text-elongate" : "nav-text-normal"}>Publications</span>
                     </Link>
                 </li>
             </ul>

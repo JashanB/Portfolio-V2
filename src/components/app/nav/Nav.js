b9a9948524ced8652563d20e1678eea319aca6d7
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-22 13:14:15 -0500

    changed functions to props

--- src/components/app/nav/Nav.js
+++ src/components/app/nav/Nav.js
@@ -4,87 +4,87 @@ import { debounce } from 'lodash'
 import './Nav.css';
 
 export default function Nav(props) {
-    const [hoveredNav, setHoveredNav] = useState(null);
-    const [activeSection, setActiveSection] = useState(null);
-    const [isNavigationClick, setIsNavigationClick] = useState(false);
+    // const [hoveredNav, setHoveredNav] = useState(null);
+    // const [activeSection, setActiveSection] = useState(null);
+    // const [isNavigationClick, setIsNavigationClick] = useState(false);
 
-    function hoverNav(section) {
-        setHoveredNav(state => section);
-    }
+    // function hoverNav(section) {
+    //     setHoveredNav(state => section);
+    // }
 
-    function moveOffNav(section) {
-        if (activeSection !== section) {
-            setHoveredNav(state => null);
-        }
-    }
+    // function moveOffNav(section) {
+    //     if (activeSection !== section) {
+    //         setHoveredNav(state => null);
+    //     }
+    // }
 
-    const handleSetActive = useCallback((to) => {
-        if (!isNavigationClick) {
-            setActiveSection(state => to);
-            setHoveredNav(state => null);
-        }
-    }, [isNavigationClick, setActiveSection, setHoveredNav]);
+    // const handleSetActive = useCallback((to) => {
+    //     if (!isNavigationClick) {
+    //         setActiveSection(state => to);
+    //         setHoveredNav(state => null);
+    //     }
+    // }, [isNavigationClick, setActiveSection, setHoveredNav]);
 
-    function selectedNavItem(section) {
-        if (activeSection === section || hoveredNav === section) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // function selectedNavItem(section) {
+    //     if (activeSection === section || hoveredNav === section) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
-    function handleLinkClick(section) {
-        setIsNavigationClick(true);
-        //Stop in between nav items being set to active as page scrolls by from nav bar click
-        setTimeout(() => {
-            setIsNavigationClick(false);
-        }, 500);
-        handleSetActive(section)
-    };
+    // function handleLinkClick(section) {
+    //     setIsNavigationClick(true);
+    //     //Stop in between nav items being set to active as page scrolls by from nav bar click
+    //     setTimeout(() => {
+    //         setIsNavigationClick(false);
+    //     }, 500);
+    //     handleSetActive(section)
+    // };
 
-    useEffect(() => {
-        //set about to active on page render 
-        handleSetActive('about');
-    }, [handleSetActive])
+    // useEffect(() => {
+    //     //set about to active on page render 
+    //     handleSetActive('about');
+    // }, [handleSetActive])
 
     return (
         <nav className={!props.smallScreen ? "nav-hidden" : "nav-open"}>
             <ul className='nav-ul'>
-                <li className={selectedNavItem('about') ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav('about')}
-                    onMouseLeave={() => moveOffNav('about')}
+                <li className={props.selectedNavItem('about') ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => props.hoverNav('about')}
+                    onMouseLeave={() => props.moveOffNav('about')}
 
                 >
-                    <Link spy={true} smooth={true} offset={-100} duration={500} to="about" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive} onClick={() => handleLinkClick('about')}>
-                        <span className={selectedNavItem('about') ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={selectedNavItem('about') ? "nav-text-elongate" : "nav-text-normal"}>About</span>
+                    <Link spy={true} smooth={true} offset={-100} duration={500} to="about" activeClass="active" className='flex-nav-center' onSetActive={props.handleSetActive} onClick={() => props.handleLinkClick('about')}>
+                        <span className={props.selectedNavItem('about') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={props.selectedNavItem('about') ? "nav-text-elongate" : "nav-text-normal"}>About</span>
                     </Link>
                 </li>
-                <li className={selectedNavItem('experience') ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav('experience')}
-                    onMouseLeave={() => moveOffNav('experience')}
+                <li className={props.selectedNavItem('experience') ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => props.hoverNav('experience')}
+                    onMouseLeave={() => props.moveOffNav('experience')}
                 >
-                    <Link spy={true} smooth={true} offset={-60} duration={500} to="experience" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive} onClick={() => handleLinkClick('experience')}>
-                        <span className={selectedNavItem('experience') ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={selectedNavItem('experience') ? "nav-text-elongate" : "nav-text-normal"}>Experience</span>
+                    <Link spy={true} smooth={true} offset={-60} duration={500} to="experience" activeClass="active" className='flex-nav-center' onSetActive={props.handleSetActive} onClick={() => props.handleLinkClick('experience')}>
+                        <span className={props.selectedNavItem('experience') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={props.selectedNavItem('experience') ? "nav-text-elongate" : "nav-text-normal"}>Experience</span>
                     </Link>
                 </li>
-                <li className={selectedNavItem('projects') ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav('projects')}
-                    onMouseLeave={() => moveOffNav('projects')}
+                <li className={props.selectedNavItem('projects') ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => props.hoverNav('projects')}
+                    onMouseLeave={() => props.moveOffNav('projects')}
                 >
-                    <Link spy={true} smooth={true} offset={-60} duration={500} to="projects" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive} onClick={() => handleLinkClick('projects')}>
-                        <span className={selectedNavItem('projects') ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={selectedNavItem('projects') ? "nav-text-elongate" : "nav-text-normal"}>Projects</span>
+                    <Link spy={true} smooth={true} offset={-60} duration={500} to="projects" activeClass="active" className='flex-nav-center' onSetActive={props.handleSetActive} onClick={() => props.handleLinkClick('projects')}>
+                        <span className={props.selectedNavItem('projects') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={props.selectedNavItem('projects') ? "nav-text-elongate" : "nav-text-normal"}>Projects</span>
                     </Link>
                 </li>
-                <li className={selectedNavItem('publications') ? "hovered-nav-item" : "nav-item-normal"}
-                    onMouseEnter={() => hoverNav('publications')}
-                    onMouseLeave={() => moveOffNav('publications')}
+                <li className={props.selectedNavItem('publications') ? "hovered-nav-item" : "nav-item-normal"}
+                    onMouseEnter={() => props.hoverNav('publications')}
+                    onMouseLeave={() => props.moveOffNav('publications')}
                 >
-                    <Link spy={true} smooth={true} offset={-50} duration={500} to="publications" activeClass="active" className='flex-nav-center' onSetActive={handleSetActive} onClick={() => handleLinkClick('publications')}>
-                        <span className={selectedNavItem('publications') ? "nav-line-elongate" : "nav-line-normal"}></span>
-                        <span className={selectedNavItem('publications') ? "nav-text-elongate" : "nav-text-normal"}>Publications</span>
+                    <Link spy={true} smooth={true} offset={-50} duration={500} to="publications" activeClass="active" className='flex-nav-center' onSetActive={props.handleSetActive} onClick={() => props.handleLinkClick('publications')}>
+                        <span className={props.selectedNavItem('publications') ? "nav-line-elongate" : "nav-line-normal"}></span>
+                        <span className={props.selectedNavItem('publications') ? "nav-text-elongate" : "nav-text-normal"}>Publications</span>
                     </Link>
                 </li>
             </ul>
